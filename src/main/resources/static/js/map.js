82283682ca4449ad5893dd0fc8a3807674c728d4
Cerdure <110950838+Cerdure@users.noreply.github.com>
2022-12-30 01:19:26 +0900

    채팅방 view 작성 중

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -6,4 +6,13 @@ $(function () {
    $(".apart-detail .close-btn").click(function(){
     $(".apart-detail-wrapper").stop().fadeOut(100);
    });
+   $(".trend-wrapper .fold").click(function(){
+    if(!$(this).hasClass("clicked")){
+        $(this).addClass("clicked").css('transform', 'translateY(-50%) rotate(180deg)');
+        $(".trend-wrapper").css('overflow','visible');
+    } else {
+        $(this).removeClass("clicked").css('transform', 'translateY(-50%) rotate(0)');
+        $(".trend-wrapper").css('overflow','hidden');
+    }
+   });
 });
