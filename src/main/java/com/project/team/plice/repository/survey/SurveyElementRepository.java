07d790f313ea3af8b3d7fc7fb6bc86896dbfede4
Cerdure <110950838+Cerdure@users.noreply.github.com>
2023-01-13 18:20:32 +0900

    채팅 날짜 그룹 추가, 최근 채팅 추가

--- src/main/java/com/project/team/plice/repository/survey/SurveyElementRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.team.plice.repository.survey;
-
-import com.project.team.plice.domain.survey.SurveyElement;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SurveyElementRepository extends JpaRepository<SurveyElement, Long> {
-}
