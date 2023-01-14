3b294c61dafdbb7912a10e2d0b6fb42a5fbd0ab8
Cerdure <110950838+Cerdure@users.noreply.github.com>
2023-01-14 14:01:45 +0900

    불필요한 파일 제거

--- src/main/java/com/project/team/plice/repository/survey/SurveyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.team.plice.repository.survey;
-
-import com.project.team.plice.domain.survey.Survey;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SurveyRepository extends JpaRepository<Survey, Long> {
-}
--- src/main/java/com/project/team/plice/repository/vote/VoteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.team.plice.repository.vote;
-
-import com.project.team.plice.domain.vote.Vote;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VoteRepository extends JpaRepository<Vote, Long> {
-}
